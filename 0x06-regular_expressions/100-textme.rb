cfb9f50f755785528208fdfbc468593c4ae781f7
Areejslama <Areegahmed449@gmail.com>
2024-02-27 17:21:48 -0800

    text

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-input_string = ARGV[0]
-regex_pattern = /\[(.*?),(.*?),(.*?)\]/
-matches = input_string.match(regex_pattern)
-if matches
-  sender = matches[1]
-  receiver = matches[2]
-  flags = matches[3]
-
-  puts "Sender: #{sender}"
-  puts "Receiver: #{receiver}"
-  puts "Flags: #{flags}"
-else
-  puts "Invalid input format"
-end
+puts ARGV[0].scan(/[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')

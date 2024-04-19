265744adce82c4000a5c8973c9476915096b4215
Areejslama <Areegahmed449@gmail.com>
2024-04-19 13:23:34 -0700

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # this  script to install a package
-package {'flask':
-  ensure => 'installed',
+package { 'Werkzeug':
+ensure  => '2.1.1',
+provider => 'pip3',
+}
+
+package { 'flask':
+  ensure => '2.1.0',
 provider => 'pip3',
 }

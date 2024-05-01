b6e822e23ddc7dd0cbf332278fb00a6b6e16882f
Areejslama <Areegahmed449@gmail.com>
2024-04-30 17:26:21 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,24 +2,26 @@
 # this script to puppet http header
 
 exec { 'update system':
-command => '/usr/bin/apt-get update'
+  command => '/usr/bin/apt-get update'
 }
 package { 'nginx':
-ensure  => 'present',
-require => Exec['update system']
+  ensure  => 'installed',
+  require => Exec['update system']
 }
 file { '/var/www/index/index.html':
-content  => 'Hello world'
+  content  => 'Hello world'
 }
 exec { 'redirect_me':
-command  => 'sudo sed -i "s/server_name _;/$my_string/"  /etc/nginx/sites-enabled/default',
-provider => 'shell'
+  command  => 'sed -i "241s/.*/rewrite ^\/redirect_me https:\/\/github.com\/areejslama permanent;/" /etc/nginx/sites-enabled/default',
+  path     => '/bin:/usr/bin',
+  require  => Package['nginx']
 }
 exec { 'HTTP header':
-command  => 'sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
-provider => 'shell'
+  command  => 'sed -i "251s/.*/add_header X-Served-By $HOSTNAME;/" /etc/nginx/sites-enabled/default',
+   path     => '/bin:/usr/bin',
+  require  => Package['nginx']
 }
 service { 'nginx':
-ensure  => 'present',
-require => package['nginx']
+  ensure  => 'running',
+  require => Package['nginx']
 }

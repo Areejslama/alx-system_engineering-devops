4621a2a33ba54d736ada9ae381b859706088e1ed
Areejslama <Areegahmed449@gmail.com>
2024-05-20 16:56:12 -0700

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """this script to gather data"""
+import csv
 import requests
 import sys
-import csv
 
 
 if __name__ == "__main__":
@@ -11,13 +11,8 @@ if __name__ == "__main__":
     employee = requests.get(url + "users/{}".format(employee_id)).json()
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
     complete = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee.get("name"), len(complete), len(todos)))
-    [print("\t {}".format(i)) for i in complete]
     with open ("{}.csv".format(employee_id), "w")as f:
-        write = csv.writer(f)
-        write.writerow(['USER_ID', 'USERNAME', 
-            'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        write = csv.writer(f,  quoting=csv.QUOTE_ALL)
         for t in todos:
             write.writerow([employee_id, employee.get("username"),
                 t.get("completed"), t.get("title")])

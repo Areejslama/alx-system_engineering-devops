26896c2e02681c026dbd1be58af7b355ecbc52f8
Areejslama <Areegahmed449@gmail.com>
2024-05-20 18:16:21 -0700

    2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this script to gather data"""
+import csv
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    employee = requests.get(url + "users/{}".format(employee_id)).json()
+    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    complete = [t.get("title") for t in todos if t.get("completed") is True]
+    with open("{}.json".format(employee_id), "w") as f:
+        json.dump({
+            "employee_id": [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": employee.get("username")
+            } for t in todos]
+        }, f)

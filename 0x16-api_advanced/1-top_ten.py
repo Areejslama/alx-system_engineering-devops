1a6473c41d28ebd0d2f4efe4be6786332776d359
Areejslama <Areegahmed449@gmail.com>
2024-06-04 16:30:34 -0700

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
         return None
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200
+    if res.status_code != 200:
         return None
     data = res.json().get('data', {})
     children = data.get('children', [])

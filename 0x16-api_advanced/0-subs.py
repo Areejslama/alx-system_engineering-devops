32e70b81a60bef27cd88198653efc9a724c658d7
Areejslama <Areegahmed449@gmail.com>
2024-06-04 14:31:03 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None:
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    res = requests.get(url, headers=headers,allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
         return 0
     return res.json()['data']['subscribers']

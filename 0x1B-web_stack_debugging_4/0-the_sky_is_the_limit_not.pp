c0730b5441cbdacd8125a0f4eb970808c5a9e60b
Areejslama <Areegahmed449@gmail.com>
2024-06-13 19:30:50 -0700

    4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,12 @@
 
 # Define an exec resource to modify Nginx configuration
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" etc/default/nginx && sudo service nginx restart' ,
-  path    => ['/usr/local/bin', '/bin']
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }

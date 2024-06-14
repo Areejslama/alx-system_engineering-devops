bd8ffa822a988fcf84173938f6185107db1d61ba
Areejslama <Areegahmed449@gmail.com>
2024-06-13 18:02:29 -0700

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,21 @@
-# this mainfest is about setting up server  
+# This manifest sets up a server and applies necessary configurations to Nginx.
+
+# Define an exec resource to modify Nginx configuration
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/nginx/default',
-  path    => '/usr/local/bin/:/bin/'
-exec ( 'nginx restart':
-  command => 'nginx restart',
-  path => '/etc/init.d/'
+  command => 'sed -i "s/15/4096/" /etc/nginx/nginx.conf',
+  path    => ['/usr/local/bin', '/bin']
+
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => ['/sbin', '/usr/sbin', '/bin', '/usr/bin']
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  subscribe  => Exec['fix--for-nginx'],
+}
